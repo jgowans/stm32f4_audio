62db0211f39abec96c0e9372270b1422d8888946
James Gowans <gowans.james@gmail.com>
2015-07-22 15:46:25 +0200

    Attempt to reduce pop with delay and 0 offset

--- audio_extractor.py
+++ audio_extractor.py
@@ -7,10 +7,10 @@ with open('klaxon.raw', 'rb') as f:
     raw = f.read()
 audio = np.frombuffer(raw, dtype=np.int16)
 audio = audio * (512.0/25000)
-audio = audio + 512
-audio = audio.clip(0, 1023)
+audio = audio
+audio = audio.clip(-1023, 1023)
 with open("klaxon.h", 'w') as f:
-    f.write("#include <stdint.h>\nuint16_t klaxon_array = { ")
+    f.write("#include <stdint.h>\nconst int16_t klaxon_array[] = { ")
     for idx, val in enumerate(audio):
         if idx%15 == 0:
             f.write('\n')
--- main.c
+++ main.c
@@ -14,8 +14,8 @@ int main(void) {
     if (0 != BSP_AUDIO_OUT_Init(OUTPUT_DEVICE_HEADPHONE, 100, 44100)) {
         for(;;);
     }
-    cs43l22_Play(AUDIO_I2C_ADDRESS, NULL, 0);
     __HAL_I2S_ENABLE(&hAudioOutI2s);
+    cs43l22_Play(AUDIO_I2C_ADDRESS, NULL, 0);
     for(sample_cnt = 0; sample_cnt < sizeof(klaxon_array)/(sizeof(klaxon_array[0])) ; ++sample_cnt) {
         if (I2S_WaitFlagStateUntilTimeout(&hAudioOutI2s, I2S_FLAG_TXE, RESET, 100) != HAL_OK) {
             for(;;);
@@ -27,6 +27,13 @@ int main(void) {
         (hAudioOutI2s.Instance)->DR = klaxon_array[sample_cnt];
     }
     cs43l22_Stop(AUDIO_I2C_ADDRESS, 0);
+    for(sample_cnt = 0; sample_cnt < 100000; ++sample_cnt) {
+        (hAudioOutI2s.Instance)->DR = klaxon_array[sample_cnt];
+        if (I2S_WaitFlagStateUntilTimeout(&hAudioOutI2s, I2S_FLAG_TXE, RESET, 100) != HAL_OK) {
+            for(;;);
+        }
+        (hAudioOutI2s.Instance)->DR = 0;
+    }
     for(;;);
     return 0;
 }
